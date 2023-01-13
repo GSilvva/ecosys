bb8d965437b5b022f9875c54198e572376046b07
Gabriel da Silva <hello.gabrielsilva2@gmail.com>
2023-01-13 14:22:24 -0300

    layout

--- pages/comprar/index.vue
+++ pages/comprar/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <NuxtLayout>
+    <NuxtLayout :name="layout">
         <main class="cars block xl:grid pt-sm">
             <aside
                 :class="`transition filters bg-white overflow-y-auto fixed xl:relative z-40 xl:z-10 top-0 left-0 w-full opacity-0 invisible xl:opacity-100 xl:visible ${buyPage.filtersActive ? 'active' : ''}`"
@@ -255,7 +255,7 @@ useHead({
     }
 })
 
-definePageMeta({ layout: 'simple-full' })
+const layout = "simple-full"
 </script>
 
 <style lang="scss" scoped>
